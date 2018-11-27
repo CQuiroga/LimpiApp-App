6c97e7e04ef0fb55f2cbba579904e5683890901e
Camilo <camilo.quirogam@gmail.com>
2018-11-27 00:18:47 -0500

    Modificación de backgrounds y registro de "ecoaliados" ok

--- src/pages/ecoaliados/ecoaliados.ts
+++ src/pages/ecoaliados/ecoaliados.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,LoadingController, AlertController, ToastController } from 'ionic-angular';
 import { HomePage } from '../home/home';
-/**
- * Generated class for the EcoaliadosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -16,12 +12,93 @@ import { HomePage } from '../home/home';
 export class EcoaliadosPage {
 
   HomePage = HomePage;
+  registros: any[] = []; // variable para guardar los registros
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alerta: AlertController,
+    private toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EcoaliadosPage');
+    console.log('Ecoaliados ok');
   }
+
+  nuevoEcoaliado(){
+  let alerta = this.alerta.create({
+    title: "Registro de ecoaliados",
+    message: "Por favor ingresa tus datos",
+    inputs: [
+      {
+        type: "text",
+        placeholder: "Nombre u organización:", // texto por defecto del input
+        name: "nombre" // para el data binding
+      },
+      {
+        type: "text",
+        placeholder: "Cédula o NIT:",
+        name: "cedula"
+      },
+      {
+        type: "text",
+        placeholder: "Dirección:",
+        name: "direccion"
+      },
+      {
+        type: "text",
+        placeholder: "Correo:",
+        name: "correo"
+      },
+      {
+        type: "text",
+        placeholder: "Teléfono:",
+        name: "telefono"
+      },
+      {
+        type: "string",
+        placeholder: "Mensaje/comentarios:",
+        name: "mensaje"
+      }
+    ],
+    buttons: [
+      {
+        text: "Cancelar",
+        handler: () => {} // Funcion que se ejecuta al pulsar el boton, este boton no hará nada
+      },
+      {
+        text: "Ser Ecoaliado",
+        handler: (datos) => {
+          firebase.firestore().collection("ecoaliados").add({
+            nombre: datos.nombre ,
+            cedula: datos.cedula,
+            direccion: datos.direccion,
+            correo: datos.correo,
+            telefono: datos.telefono,
+            mensaje: datos.mensaje
+          })
+          .then((documento) => {
+            console.log('ecoaliado ' + datos.nombre + ' creado satisfactoriamente');
+            // crea toast de confirmación
+            const toast = this.toastCtrl.create({
+              message: '¡Te has registrado como ecoaliado!',
+              showCloseButton: true,
+              closeButtonText: 'Ok'
+            });
+            toast.onDidDismiss(this.dismissHandler);
+            toast.present();
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+        }
+      }
+    ]
+  });
+  alerta.present();
+}
+
+private dismissHandler() {
+   console.info('ok');
+}
 
 }
